a893881fe14eabd38363eede83b9c389a24ef9dd
Douglasjrdev <dougfernandesalves09@gmail.com>
2023-09-26 22:14:32 -0300

    fiz algumas att em js

--- curso do telegra/js/objeto.js
+++ curso do telegra/js/objeto.js
@@ -1,5 +1,47 @@
-var aluno = { nome: "Douglas", notas:[7.5, 5.0]
+// var aluno = { nome: "Douglas", notas:[7.5, 5.0]
 
-}
+// }
 
-console.log(aluno)
+// console.log(aluno)
+
+//SOMAS
+
+// var n1 = prompt("Digite um numero: ");
+// n1 = parseInt(n1);
+
+// var n2 = prompt("Digite outro numero? ");
+// n2 = parseInt(n2);
+
+// alert("A multiplicação é: " + (n1 * n2) )
+
+// alert("A Divisão é: " + (n1 / n2));
+
+// alert("A Subtração é: " + (n1 - n2));
+
+// alert("A Soma é: " + (n1 + n2));
+
+
+//MEDIAS
+
+// var n1 = prompt("Digite um numero: ");
+// n1 = parseInt(n1);
+
+// var n2 = prompt("Digite outro numero? ");
+// n2 = parseInt(n2);
+
+// var media = (n1 + n2) / 2;
+
+// alert(media);
+
+// desafio mostrar resto da divisão entre os numeros
+
+var n1 = prompt("Digite um numero: ");
+n1 = parseInt(n1);
+
+var n2 = prompt("Digite outro numero? ");
+n2 = parseInt(n2);
+
+var resultado = n1 % n2;
+
+alert("O resto da divisão de: " + n1 + " por "+ n2 + " é " + resultado);
+
